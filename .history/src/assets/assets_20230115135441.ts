ece00fcdcbdb9b57320118887b1f9c5037e725c2
jasurbek <94533241+Jasurbek002@users.noreply.github.com>
2023-01-15 16:53:27 +0500

    updated

--- .history/src/assets/assets_20230115135441.ts
+++
@@ -1,9 +0,0 @@
-import hh from './images/hh.png'
-import asmo from './images/asmo.png'
-import tdtu from './images/tdtu.png'
-
-export const pro = {
-    hil:hh,
-    asmoc:asmo,
-    tdtuico:tdtu
-}
